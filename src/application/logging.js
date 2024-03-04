409613959129a30853f974e79a34d677b052d932
Ardhi Harry <ardhiharry@gmail.com>
2024-03-05 00:58:17 +0700

    add new logs

--- src/application/logging.js
+++ src/application/logging.js
@@ -8,4 +8,20 @@ const logger = winston.createLogger({
   ],
 });
 
-export default logger;
+const logs = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'DD/MMM/YYYY:HH:mm:ss Z',
+    }),
+    winston.format.printf((info) => `${process.env.APP_HOST} - [${info.timestamp}] - ${info.level}: "${info.message}"`),
+  ),
+  transports: [
+    new winston.transports.Console({}),
+  ],
+});
+
+export {
+  logger,
+  logs,
+};
