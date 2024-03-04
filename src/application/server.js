07a32638e4d87f1657a4d3e421b5ceca47671658
Ardhi Harry <ardhiharry@gmail.com>
2024-03-05 00:59:14 +0700

    add public route and error middleware

--- src/application/server.js
+++ src/application/server.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import publicRoute from '../route/public.route.js';
+import errorMiddleware from '../middleware/error.middleware.js';
 
 const server = express();
+
 server.use(express.json());
+server.use(publicRoute);
+server.use(errorMiddleware);
+
+export default server;
