f6776f17992513ec083bfc5ae2c044426555cb76
Ardhi Harry <ardhiharry@gmail.com>
2024-03-08 22:27:13 +0700

    add user route in server

--- src/application/server.js
+++ src/application/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import publicRoute from '../route/public.route.js';
+import userRoute from '../route/user.route.js';
 import errorMiddleware from '../middleware/error.middleware.js';
 
 const server = express();
-
 server.use(express.json());
+
 server.use(publicRoute);
+server.use(userRoute);
+
 server.use(errorMiddleware);
 
 export default server;
