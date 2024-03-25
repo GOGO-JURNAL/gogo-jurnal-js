0b34c062426af7ed88bc32e03bb3f11380f99b1c
Ardhi Harry <ardhiharry@gmail.com>
2024-03-25 23:51:51 +0700

    move university, prodi, and dosen route to user route

--- src/route/public.route.js
+++ src/route/public.route.js
@@ -1,34 +1,17 @@
 import express from 'express';
 import userController from '../user/user.controller.js';
-import universityController from '../university/university.controller.js';
 import prodiController from '../prodi/prodi.controller.js';
 import dosenController from '../dosen/dosen.controller.js';
-import journalController from '../riset/journal.controller.js';
+import journalController from '../journal/journal.controller.js';
 
 const publicRoute = new express.Router();
 
+// User API
 publicRoute.post('/api/users', userController.register);
 publicRoute.post('/api/users/login', userController.login);
 
-publicRoute.post('/api/university', universityController.create);
-publicRoute.get('/api/university', universityController.getAll);
-publicRoute.get('/api/university/:id', universityController.get);
-publicRoute.patch('/api/university/:id', universityController.update);
-publicRoute.delete('/api/university/:id', universityController.destroy);
-
-publicRoute.post('/api/prodi', prodiController.create);
-publicRoute.get('/api/prodi', prodiController.getAll);
-publicRoute.get('/api/prodi/:id', prodiController.get);
-publicRoute.patch('/api/prodi/:id', prodiController.update);
-publicRoute.delete('/api/prodi/:id', prodiController.destroy);
-
-publicRoute.post('/api/dosen', dosenController.create);
-publicRoute.get('/api/dosen', dosenController.getAll);
-publicRoute.get('/api/dosen/:id', dosenController.get);
-publicRoute.patch('/api/dosen/:id', dosenController.update);
-publicRoute.delete('/api/dosen/:id', dosenController.destroy);
-
-publicRoute.post('/api/riset', journalController.create);
-publicRoute.get('/api/riset', journalController.getAll);
+// Journal API
+publicRoute.post('/api/journal', journalController.create);
+publicRoute.get('/api/journal', journalController.getAll);
 
 export default publicRoute;
